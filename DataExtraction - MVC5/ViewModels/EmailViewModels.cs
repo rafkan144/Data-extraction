a8998ad1dd2eb735c13cfdecd8ac848413d4e6d1
Rafał <rafkan144@wp.pl>
2017-06-21 09:01:10 +0200

    add viewModel class for mail and edit web.config

---
+++ DataExtraction - MVC5/ViewModels/EmailViewModels.cs
@@ -0,0 +1,16 @@
+﻿using Postal;
+using DataExtraction___MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExtraction___MVC5.ViewModels
+{
+    public class DataExtractionConfirmationEmail : Email
+    {
+        public string To { get; set; }
+        public string FullAddress { get; set; }
+    }
+}
