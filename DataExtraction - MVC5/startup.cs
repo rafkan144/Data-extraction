435e42d5ef7be8ef7a88985446d83e7cdc0e50c6
Rafał <rafkan144@wp.pl>
2017-06-21 09:25:37 +0200

    create startup class

---
+++ DataExtraction - MVC5/startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+using Hangfire.SqlServer;
+using Hangfire.Dashboard;
+
+[assembly: OwinStartup(typeof(DataExtraction___MVC5.Startup))]
+
+namespace DataExtraction___MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+
+            app.UseHangfire(config =>
+            {
+                //config.UseSqlServerStorage("StoreContext");
+                //config.UseServer();
+            });
+        }
+    }
+}
